33bd2015e7a92a03a0db611124541b94bdbcda89
Henrry Tien (Dao Tien) <58262966+DTien93@users.noreply.github.com>
2024-01-08 23:12:13 +0700

    Update code dark_mode_light_react

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,61 @@ import React from 'react'
 import Board from './pages/Bords/_id'
 import Button from '@mui/material/Button'
 import { useColorScheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Box from '@mui/material/Box'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightness from '@mui/icons-material/SettingsBrightness'
+
+
+function ModelSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+  return (
+    <Box sx={{ m:1, minWidth: 120 }} size='small'>
+      <FormControl fullWidth>
+        <InputLabel id='label-selecl-dark-ligh-mode'>Mode</InputLabel>
+        <Select
+          labelId='label-selecl-dark-ligh-mode'
+          id='demo-simple-select'
+          value={mode}
+          label='Mode'
+          onChange={handleChange}
+        >
+          <MenuItem value='ligh'>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <LightModeIcon fontSize='small'/> Light
+            </Box>
+          </MenuItem>
+          <MenuItem value='dark'>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <DarkModeOutlinedIcon fontSize='small'/> Dark
+            </Box>
+          </MenuItem>
+          <MenuItem value='system'>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <SettingsBrightness fontSize='small'/> System
+            </Box>
+          </MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  )
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  const prefersLighMode = useMediaQuery('(prefers-color-scheme: light)')
+
   return (
     <Button
       onClick={() => {
@@ -20,6 +72,7 @@ function App() {
   return (
     <React.Fragment>
       <Board />
+      <ModelSelect />
       <hr/>
       <ModeToggle />
     </React.Fragment>
